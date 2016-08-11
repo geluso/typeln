fcba22e1ac2ab79543acbe6eef152e5a61261847
Steve Geluso <stevegeluso@gmail.com>
2016-08-10 22:37:36 -0700

    removing console logs from recorder

--- js/recorder.js
+++ js/recorder.js
@@ -3,13 +3,11 @@ var POINTS = [];
 
 
 function startRecord() {
-  console.log("start recording");
   RECORDING = true;
   POINTS = [];
 }
 
 function stopRecord() {
-  console.log("stop recording");
   RECORDING = false;
   drawLine(POINTS);
 }
@@ -19,6 +17,5 @@ function record() {
     return;
   }
 
-  console.log("recording", MOUSE_XX, MOUSE_YY);
   POINTS.push(new Point(MOUSE_XX, MOUSE_YY));
 }
