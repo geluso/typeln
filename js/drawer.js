c4a862ff956c0697145c82f5f6a86fcdf3f1f1a0
Steve Geluso <stevegeluso@gmail.com>
2016-08-25 17:49:50 +0800

    using chart js

--- js/drawer.js
+++ js/drawer.js
@@ -27,5 +27,53 @@ function drawLine(points) {
     });
   }, 100);
 
+  // create a chart of line slopes
+  var chartCtx = document.getElementById("myChart").getContext("2d");
+  var pieData = [
+      {
+          value: 20,
+          color:"#878BB6"
+      },
+      {
+          value : 40,
+          color : "#4ACAB4"
+      },
+      {
+          value : 10,
+          color : "#FF8153"
+      },
+      {
+          value : 30,
+          color : "#FFEA88"
+      }
+  ];
+  // Get the context of the canvas element we want to select
+  
+  var xs = pointsXY.map(function(pt) {
+    return pt.x;
+  });
+
+  var ys = pointsXY.map(function(pt) {
+    return pt.y;
+  });
+
+  var lineData = {
+    labels: ys,
+    datasets: [
+        {
+            label: "Sodium intake",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: xs
+        }
+    ]
+  };
+
+  new Chart(chartCtx).Line(lineData);
+
   return pointsXY;
 }
