6489dc13867f89fcdc427eba8b7e1656b6dd5a78
Steve Geluso <stevegeluso@gmail.com>
2018-09-19 12:34:36 -0700

    gutting paper, graph

--- js/drawer.js
+++ js/drawer.js
@@ -1,68 +1,29 @@
-var DRAW_ALL_POINTS = false;
-var DRAW_CORNER_POINTS = true;
+var DRAW_ALL_POINTS = true;
 
 function drawLine(points) {
   if (points.length < 2) {
     return;
   }
 
-  console.log('cleared');
   CTX.clearRect(0, 0, WIDTH, HEIGHT);
 
-  CTX.strokeColor = "green";
+  CTX.save();
+  CTX.beginPath();
+  CTX.strokeStyle = "blue";
+  CTX.moveTo(xx(points[0]), yy(points[0]));
+
   pointsXY = _.map(points, function(point) {
     var x = xx(point.x);
     var y = yy(point.y)
 
+    CTX.strokeRect(point.x - 3, point.y - 3, 5, 5);
+    CTX.lineTo(x, y);
+
     return new Point(x, y);
   });
 
-  paper.setup(CANVAS);
-  var path = new paper.Path(pointsXY);
-  path.strokeColor = "red";
+  CTX.stroke();
+  CTX.restore();
 
-  var smoothed = path.clone()
-  smoothed.strokeColor = "blue";
-  smoothed.flatten(.2);
-
-  if (DRAW_ALL_POINTS) {
-    // set a timeout so the rects are drawn after the paper lib
-    setTimeout(function() {
-      drawAllPoints(pointsXY);
-    }, 100);
-  }
-
-  if (DRAW_CORNER_POINTS) {
-    setTimeout(function() {
-      var corners = determineCorners(pointsXY);
-      drawAllPoints(corners);
-    }, 100);
-
-  }
-
-  drawMapping();
   return pointsXY;
-}
-
-function drawAllPoints(pointsXY) {
-  _.each(pointsXY, function(point) {
-    CTX.strokeRect(point.x - 3, point.y - 3, 5, 5);
-  });
-}
-
-function determineCorners(points) {
-  if (points.length < 3) {
-    return [];
-  }
-
-  var corners = [];
-
-  var isXIncreasing = false;
-  var isYIncreasing = false;
-
-  _.each(points, function(point) {
-
-  });
-
-  return corners;
 }
